cea76a7c9f8720fc50f2484442caa029310f142e
Adrian Sims <asims@parkeon.com>
2018-02-07 10:30:00 +0000

    Add project files.

---
+++ InterDate1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InterDate1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var date = DateTime.Today;
+
+                var culture = new CultureInfo("FR-fr");
+                Thread.CurrentThread.CurrentCulture = culture;
+                Thread.CurrentThread.CurrentUICulture = culture;
+                Console.WriteLine($"date is {date:dddd dd MMM yyyy}");
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add InterDate1/Program.cs
+git add InterDate1/App.config
+git add InterDate1/Properties/AssemblyInfo.cs
+git add InterDate1/InterDate1.csproj
+git add InterDate1.sln
+git add .gitignore
